ffa5ced94b19faa5d0113dfc13537fd6ff0783fd
Leila Jacob <leilajacob@me.com>
2015-10-28 13:34:38 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def blog
+  end
+
+  def podcasts
+  end
+
+  def gallery
+  end
+
+  def knitting
+  end
+end
